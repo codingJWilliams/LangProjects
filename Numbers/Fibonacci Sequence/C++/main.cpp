6a3dd05eed8c51c4570e784fb70354822e751e8c
Jay Williams <course@jaywilliams.me>
2017-07-15 13:37:57 +0100

    Finished fibonacci in c++

--- Numbers/Fibonacci Sequence/C++/main.cpp
+++ Numbers/Fibonacci Sequence/C++/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
 #include <cstdio>
 #include <ctime>
-
+using namespace std;
 int fib(int n){
   if(n <= 1){
     return n;
   }
   return fib(n-1) + fib(n-2);
 }
-int main(){
+int main(int argc, char* argv[])
+{
+  int its =  (int)strtol(argv[1], (char **)NULL, 10);
   clock_t start;
+  cout << argv[1] << endl;
   double duration;
-
   start = clock();
-  for (int i = 0; i<40; i++){
+  for (int i = 0; i < its; i++){
     cout << fib(i) << endl;
   }
   duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-  cout << "Time taken for 40 fibonaccis: " << duration << endl;
+
 }
